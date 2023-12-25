9d0c9e1d0a0c1d5792c9decb16ac69accda6b45f
heejin <hee031812@gmail.com>
2023-12-25 21:39:51 +0900

    12-25

--- client/src/components/section/Contact.jsx
+++ client/src/components/section/Contact.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import icon1 from '../../assets/img/ICON1.png'
+import icon2 from '../../assets/img/ICON2.png'
+
 
 const Contact = () => {
+           useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const trigger = ScrollTrigger.create({
+            trigger: "#section06",
+            start: "top center",
+            end: "bottom center",
+            markers: true,
+            onEnter: () => {
+                gsap.to("body", { backgroundColor: "#1A1A1A", duration: 0.8 });
+                 gsap.to("h1", { color: "#fff", duration: 1.4 });
+      gsap.to("ul li a", { color: "#fff", duration: 1.4 });
+      gsap.to(".time", { color: "#fff", duration: 1.4 });
+      gsap.to(".contry", { color: "#fff", duration: 1.4 });
+            gsap.to("#coordinates", { color: "#fff", duration: 1.4 });
+        
+            },
+            onEnterBack: () => {
+                gsap.to("body", { backgroundColor: "#1A1A1A", duration: 0.8 });
+
+            },
+            onLeave: () => {
+                gsap.to("body", { backgroundColor: "#FF3B00", duration: 0.8 });  // onLeave에서 배경색 변경
+
+            },
+            onLeaveBack: () => {
+                gsap.to("body", { backgroundColor: "#FF3B00", duration: 0.8 });  // onLeaveBack에서 배경색 변경
+            }
+        });
+
+        return () => {
+            trigger.kill();
+        };
+    }, []);
     return (
-        <div>Contact</div>
-    )
+        <div id="section06">
+            <div className="container06">
+                <div className="Iam">
+                    <p>SAY HI !</p>
+                    <b>
+                        <div className="innerIam">
+                            leggera<br />
+                            a theme in progress<br />
+                            built on bootstrap<br />
+                            how I learn stuff<br />
+                            how we do it
+                        </div>
+                    </b>
+                </div>
+                <div className="sec06_right">
+                    <div className="contactWrap">
+                        <h6>Contact me</h6>
+                        <p>Tell us about your project.<br />
+                            Let’s collaborate and make great stuff.
+                        </p>
+                        <div className="iconWrap">
+                            <div className="icon1">
+                                <div className="iconbox"><img src={icon1} alt="GMAIL icon" /></div>
+                                <div className="iconText">GMAIL</div>
+                            </div>
+                            <div className="icon2">
+                                <div className="iconbox"><img src={icon2} alt="GIT icon" /></div>
+                                <div className="iconText">GIT</div>
+                            </div>
+                        </div>
+                        <div className="linkWrap">
+                        </div>
+                    </div>
+                    <div className="sec06_line"></div>
+                    <div className="commentWrap">
+                        <h5>Comment</h5>
+                        <p>Please write down what you want to say.</p>
+                        <fieldset>
+                            <div className="name"> <input type="text" placeholder="이름을 입력해주세요." /></div>
+                            <div className="write"> <input type="text" placeholder="댓글을 입력해주세요." /></div>
+                            <button>Send</button>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+            <div className="infoWrap">
+                <div className="git">git</div>
+                <div className="tstory">tstory</div>
+                <div className="instagram">instagram</div>
+            </div>
+            <div className="line06"></div>
+        </div>
+    );
 }
 
-export default Contact
+export default Contact;
--- client/src/components/section/Skill.jsx
+++ client/src/components/section/Skill.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+
 
 const Skill = () => {
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const trigger = ScrollTrigger.create({
+            trigger: ".skill__desc",
+            start: "top 30%",
+            end: "bottom 10%",
+            markers: true,
+            onEnter: () => {
+                gsap.to("body", { backgroundColor: "#FF3B00", duration: 0.8 });
+                gsap.to("h1", { color: "#fff", duration: 1.4 });
+                gsap.to("ul li a", { color: "#fff", duration: 1.4 });
+                gsap.to(".time", { color: "#fff", duration: 1.4 });
+                gsap.to(".country", { color: "#fff", duration: 1.4 }); 
+                gsap.to("#coordinates", { color: "#fff", duration: 1.4 });
+                gsap.to(".header", { color: "#fff", duration: 1.4 }); 
+            },
+            onEnterBack: () => {
+                gsap.to("body", { backgroundColor: "#1A1A1A", duration: 0.8 });
+                gsap.to("h1", { color: "#FF3B00", duration: 1.4 });
+                gsap.to("ul li a", { color: "#FF3B00", duration: 1.4 });
+                gsap.to(".time", { color: "#FF3B00", duration: 1.4 });
+                gsap.to(".country", { color: "#FF3B00", duration: 1.4 }); 
+                gsap.to("#coordinates", { color: "#FF3B00", duration: 1.4 });
+                gsap.to(".header", { color: "#FF3B00", duration: 1.4 }); 
+            },
+            onLeave: ({ progress }) => {
+                gsap.to("body", { backgroundColor: "#1A1A1A", duration: 1.4 });
+            },
+            onLeaveBack: ({ progress }) => {
+                if (progress < 0.5) {
+                    gsap.to("body", { backgroundColor: "#F4F0E6", duration: 1.4 });
+                    gsap.to("h1", { color: "#FF3B00", duration: 1.4 });
+                    gsap.to("ul li a", { color: "#FF3B00", duration: 1.4 });
+                    gsap.to(".time", { color: "#FF3B00", duration: 1.4 });
+                    gsap.to(".country", { color: "#FF3B00", duration: 1.4 }); // 'contry'를 'country'로 수정
+                }
+            },
+        });
+
+        return () => {
+            trigger.kill();
+        };
+    }, []);
     return (
-        <div>Skill</div>
-    )
+        <div id="section05">
+            <section id="skill">
+                <div className="skill__inner">
+                    <h2 className="skill__title">
+                        Which stack<br />
+                        do you use?
+                    </h2>
+                    <div className="skill__desc">
+                        <div>
+                            <div className="num">01.</div>
+                            <h3>Javascript</h3>
+                            <p>
+                                Javascript의 배열, 메소드, DOM을 이해하며 다양한 상황에 적용할 수 있습니다.
+                            </p>
+                        </div>
+
+                        <div className="num">02.</div>
+                        <h3>React</h3>
+                        <p>
+                            React 구조와 여러가지 hook들을 잘 알고 있으며, 범용적이고 재사용가능한 컴포넌트 설계를 할 수 있습니다.
+                        </p>
+
+                        <div className="num">03.</div>
+                        <h3>Javascript</h3>
+                        <p>
+                            Javascript의 배열, 메소드, DOM을 이해하며 다양한 상황에 적용할 수 있습니다.
+                        </p>
+
+                        <div className="num">04.</div>
+                        <h3>Vite</h3>
+                        <p>
+                            Vite 구조를 알고있으며, 재사용 가능한 컴포넌트 설계를 할 수 있습니다.
+                        </p>
+
+                        <div className="num">05.</div>
+                        <h3>Jquery</h3>
+                        <p>
+                            Jquery 사용법을 잘 알고있으며, 유지보수를 할 수 있습니다.
+                        </p>
+
+                        <div className="num">06.</div>
+                        <h3>PHP</h3>
+                        <p>
+                            PHP 구조를 알고있으며, 다양한 구현을 할 수 있습니다.
+                        </p>
+
+                        <div className="num">07.</div>
+                        <h3>NEXT.JS</h3>
+                        <p>
+                            MongoDB 어떻게 활용하는지 알고있습니다.
+                        </p>
+
+                        <div className="num">08.</div>
+                        <h3>Node.js</h3>
+                        <p>
+                            Node.js 사용하여 자유로운 표현을 할 수 있습니다.
+                        </p>
+
+                        <div className="num">09.</div>
+                        <h3>MongoDB</h3>
+                        <p>
+                            MongoDB 어떻게 활용하는지 알고있습니다.
+                        </p>
+
+                        <div className="num">10.</div>
+                        <h3>Postman</h3>
+                        <p>
+                            Postman을 이용해 서버에 GET, POST 요청을 해서 서버 디버깅을 할 수 있습니다.
+                        </p>
+
+                        <div className="num">11.</div>
+                        <h3>Sass</h3>
+                        <p>
+                            Sass 사용하여 자유로운 표현을 할 수 있습니다.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
+        
+    );
 }
 
-export default Skill
+export default Skill;
