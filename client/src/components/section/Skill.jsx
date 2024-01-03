090d89164e568d2208760d93401d99f24c000622
heejin <hee031812@gmail.com>
2024-01-04 00:09:40 +0900

    skill re

--- client/src/components/section/Skill.jsx
+++ client/src/components/section/Skill.jsx
@@ -9,7 +9,7 @@ const Skill = () => {
         gsap.registerPlugin(ScrollTrigger);
 
         const trigger = ScrollTrigger.create({
-            trigger: ".skill__desc",
+            trigger: "#section05",
             start: "top 30%",
             end: "bottom 10%",
             markers: true,
@@ -54,6 +54,10 @@ const Skill = () => {
     }, []);
     return (
         <div id="section05">
+            <div className='sec05__desc'>
+            We are a design agency based in Copenhagen. <br />With a strong belief in strategically<br /> founded and highly crafted
+            digital experiences
+            </div>
             <section id="skill">
                 <div className="skill__inner">
                     <h2 className="skill__title">
@@ -63,71 +67,69 @@ const Skill = () => {
                     <div className="skill__desc">
                         <div>
                             <div className="num">01.</div>
-                            <h3>Javascript</h3>
-                            <p>
-                                Javascript의 배열, 메소드, DOM을 이해하며 다양한 상황에 적용할 수 있습니다.
-                            </p>
+                            <h3>{"{ 자바스크립트 }"}</h3>
+                            <dd>클로저, 프로미스, 콜백, 비동기 프로그래밍과 같은 고급 자바스크립트 개념을 이해하고 효과적으로 사용할 수 있습니다.</dd>
+                            <dd>자바스크립트 기반의 프론트엔드 라이브러리나 프레임워크(예: React, Vue.js, Angular 등)를 숙련되게 사용할 수 있습니다. 이를 통해 동적이고 반응형 웹
+                                애플리케이션을 구축할 수 있습니다.</dd>
                         </div>
 
                         <div className="num">02.</div>
-                        <h3>React</h3>
-                        <p>
-                            React 구조와 여러가지 hook들을 잘 알고 있으며, 범용적이고 재사용가능한 컴포넌트 설계를 할 수 있습니다.
-                        </p>
+                        <h3>{"{ 리액트 }"}</h3>
+                       <dd>클로저, 프로미스, 콜백, 비동기 프로그래밍과 같은 고급 자바스크립트 개념을 이해하고 효과적으로 사용할 수 있습니다.</dd>
+                            <dd>자바스크립트 기반의 프론트엔드 라이브러리나 프레임워크(예: React, Vue.js, Angular 등)를 숙련되게 사용할 수 있습니다. 이를 통해 동적이고 반응형 웹
+                                애플리케이션을 구축할 수 있습니다.</dd>
 
                         <div className="num">03.</div>
-                        <h3>Javascript</h3>
-                        <p>
-                            Javascript의 배열, 메소드, DOM을 이해하며 다양한 상황에 적용할 수 있습니다.
-                        </p>
+                        <h3>{"{ 비트 }"}</h3>
+                        <dd>
+                        Vite를 사용하여 환경을 구축하는 데 경험이 있습니다. Vite 구조를 알고있으며, 재사용 가능한 컴포넌트 설계를 할 수 있습니다.</dd>
+                        <dd>다양한 프레임워크와 라이브러리와의 통합에 능숙합니다. 또한 개발 속도와 효율성을 크게 향상시킬 수 있습니다.</dd>
+                       
 
                         <div className="num">04.</div>
-                        <h3>Vite</h3>
-                        <p>
-                            Vite 구조를 알고있으며, 재사용 가능한 컴포넌트 설계를 할 수 있습니다.
-                        </p>
+                        <h3>{"{ 제이쿼리 }"}</h3>
+                       <dd>
+                        웹 개발에서 jQuery를 사용해 웹사이트에 동적인 요소와 효과를 추가하는 데 능숙합니다. DOM 조작과 이벤트 핸들링을 간편하게 해결하죠.</dd>
+                        <dd>AJAX를 사용한 비동기 통신으로 사용자 경험을 향상시키는 것에도 숙련되어 있습니다. 크로스 브라우저 호환성 문제도 잘 해결해요.</dd>
 
                         <div className="num">05.</div>
-                        <h3>Jquery</h3>
-                        <p>
-                            Jquery 사용법을 잘 알고있으며, 유지보수를 할 수 있습니다.
-                        </p>
+                        <h3>{"{ 피에치피 }"}</h3>
+                        <dd>
+                        웹 서버 측 스크립팅 언어인 PHP를 사용하여 다양한 웹 애플리케이션과 서비스를 개발했습니다. </dd>
+                        <dd>데이터베이스 통합, API 개발, 사용자 인증 등 다양한 백엔드 작업에 숙련되어 있습니다.</dd>
+
 
                         <div className="num">06.</div>
-                        <h3>PHP</h3>
-                        <p>
-                            PHP 구조를 알고있으며, 다양한 구현을 할 수 있습니다.
-                        </p>
+                        <h3>{"{ 몽고디비 }"}</h3>
+                         <dd>
+                       NoSQL 데이터베이스인 MongoDB를 사용하여 대규모 데이터 관리에 능숙합니다. 유연한 데이터 모델링과 빠른 쿼리 성능을 제공하죠.</dd>
+                        <dd>데이터 저장, 검색, 관리 등에서 높은 성능과 확장성을 보여줍니다.</dd>
+
 
                         <div className="num">07.</div>
-                        <h3>NEXT.JS</h3>
-                        <p>
-                            MongoDB 어떻게 활용하는지 알고있습니다.
-                        </p>
+                        <h3>{"{ 넥스트 제이에스 }"}</h3>
+                  <dd>
+                      서버 사이드 렌더링과 정적 사이트 생성을 위해 Next.js를 사용합니다. SEO 최적화와 빠른 로딩 시간을 제공해요.</dd>
+                        <dd>이러한 다양한 라이브러리를 사용하여 최적화된 환경을 만들수 있습니다.</dd>
+
 
                         <div className="num">08.</div>
-                        <h3>Node.js</h3>
-                        <p>
-                            Node.js 사용하여 자유로운 표현을 할 수 있습니다.
-                        </p>
+                        <h3>{"{ 노드 제이에스 }"}</h3>
+                  <dd>
+                      Node.js를 사용하여 효율적이고 확장 가능한 서버 사이드 애플리케이션을 구축합니다. 비동기 이벤트 주도 아키텍처를 활용해 높은 처리량을 달성하죠.</dd>
+                        <dd>RESTful API, 실시간 통신, 서버 로직 구현 등 다양한 백엔드 작업에 능숙합니다.</dd>
 
                         <div className="num">09.</div>
-                        <h3>MongoDB</h3>
-                        <p>
-                            MongoDB 어떻게 활용하는지 알고있습니다.
-                        </p>
+                        <h3>{"{ 포스트맨 }"}</h3>
+                     <dd>
+                      API 개발과 테스트에 Postman을 사용합니다. 다양한 HTTP 요청을 쉽게 구성하고 테스트해요.</dd>
+                        <dd>API 문서화, 테스트 자동화 등의 고급 기능을 통해 API 개발 과정을 효율화합니다.</dd>
 
                         <div className="num">10.</div>
-                        <h3>Postman</h3>
-                        <p>
-                            Postman을 이용해 서버에 GET, POST 요청을 해서 서버 디버깅을 할 수 있습니다.
-                        </p>
-
-                        <div className="num">11.</div>
-                        <h3>Sass</h3>
-                        <p>
-                            Sass 사용하여 자유로운 표현을 할 수 있습니다.
-                        </p>
+                        <h3>{"{ 사스 }"}</h3>
+                        <dd>
+                     CSS 전처리기인 Sass를 사용하여 효율적이고 재사용 가능한 스타일시트를 작성합니다. 더 깔끔하고 구조화된 CSS 코드를 작성하죠.</dd>
+                        <dd>변수, 믹스인, 중첩 등의 기능을 활용해 더 동적이고 관리하기 쉬운 스타일시트를 만듭니다.</dd>
                     </div>
                 </div>
             </section>
